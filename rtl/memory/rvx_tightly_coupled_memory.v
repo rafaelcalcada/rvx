3a7f5f1cbb00494079dbd1932ee138c5a0f856c1
Rafael Calcada <rafaelcalcada@gmail.com>
2025-12-13 20:58:50 +0000

    Rename RAM to Tightly Coupled Memory

--- rtl/memory/rvx_tightly_coupled_memory.v
+++ rtl/memory/rvx_tightly_coupled_memory.v
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2020-2025 RVX Project Contributors
 
-module rvx_ram #(
+module rvx_tightly_coupled_memory #(
 
     // Memory size in bytes
-    parameter MEMORY_SIZE = 8192,
+    parameter MEMORY_SIZE_IN_BYTES = 8192,
 
     // File with program and data
-    parameter MEMORY_INIT_FILE = ""
+    parameter MEMORY_INIT_FILE_PATH = ""
 
 ) (
 
@@ -37,48 +37,46 @@ module rvx_ram #(
 
 );
 
-  wire        reset_internal;
+  reg  [31:0] tcm                     [0:(MEMORY_SIZE_IN_BYTES/4)-1];
+
+  // verilator lint_off UNUSEDSIGNAL
   wire [31:0] port0_effective_address;
   wire [31:0] port1_effective_address;
+  // verilator lint_on UNUSEDSIGNAL
+
   wire        port0_invalid_address;
   wire        port1_invalid_address;
 
-  reg         reset_reg;
-  reg  [31:0] ram                     [0:(MEMORY_SIZE/4)-1];
-
-  always @(posedge clock) reset_reg <= !reset_n;
-
-  assign reset_internal        = !reset_n | reset_reg;
-  assign port0_invalid_address = $unsigned(port0_address) >= $unsigned(MEMORY_SIZE);
-  assign port1_invalid_address = $unsigned(port1_address) >= $unsigned(MEMORY_SIZE);
+  assign port0_invalid_address = $unsigned(port0_address) >= $unsigned(MEMORY_SIZE_IN_BYTES);
+  assign port1_invalid_address = $unsigned(port1_address) >= $unsigned(MEMORY_SIZE_IN_BYTES);
 
   integer i;
   initial begin
-    for (i = 0; i < MEMORY_SIZE / 4; i = i + 1) ram[i] = 32'h00000000;
-    if (MEMORY_INIT_FILE != "") $readmemh(MEMORY_INIT_FILE, ram);
+    for (i = 0; i < MEMORY_SIZE_IN_BYTES / 4; i = i + 1) tcm[i] = 32'h00000000;
+    if (MEMORY_INIT_FILE_PATH != "") $readmemh(MEMORY_INIT_FILE_PATH, tcm);
   end
 
   assign port0_effective_address = $unsigned(port0_address[31:0] >> 2);
   assign port1_effective_address = $unsigned(port1_address[31:0] >> 2);
 
   always @(posedge clock) begin
-    if (reset_internal | port0_invalid_address) port0_rdata <= 32'h00000000;
-    else port0_rdata <= ram[port0_effective_address];
-    if (reset_internal | port1_invalid_address) port1_rdata <= 32'h00000000;
-    else port1_rdata <= ram[port1_effective_address];
+    if (!reset_n | port0_invalid_address) port0_rdata <= 32'h00000000;
+    else port0_rdata <= tcm[port0_effective_address];
+    if (!reset_n | port1_invalid_address) port1_rdata <= 32'h00000000;
+    else port1_rdata <= tcm[port1_effective_address];
   end
 
   always @(posedge clock) begin
     if (port0_wrequest) begin
-      if (port0_wstrobe[0]) ram[port0_effective_address][7:0] <= port0_wdata[7:0];
-      if (port0_wstrobe[1]) ram[port0_effective_address][15:8] <= port0_wdata[15:8];
-      if (port0_wstrobe[2]) ram[port0_effective_address][23:16] <= port0_wdata[23:16];
-      if (port0_wstrobe[3]) ram[port0_effective_address][31:24] <= port0_wdata[31:24];
+      if (port0_wstrobe[0]) tcm[port0_effective_address][7:0] <= port0_wdata[7:0];
+      if (port0_wstrobe[1]) tcm[port0_effective_address][15:8] <= port0_wdata[15:8];
+      if (port0_wstrobe[2]) tcm[port0_effective_address][23:16] <= port0_wdata[23:16];
+      if (port0_wstrobe[3]) tcm[port0_effective_address][31:24] <= port0_wdata[31:24];
     end
   end
 
   always @(posedge clock) begin
-    if (reset_internal) begin
+    if (!reset_n) begin
       port0_rresponse <= 1'b0;
       port1_rresponse <= 1'b0;
       port0_wresponse <= 1'b0;
@@ -89,8 +87,5 @@ module rvx_ram #(
       port0_wresponse <= port0_wrequest;
     end
   end
-
-  // Avoid warnings about intentionally unused pins/wires
-  wire unused_ok = &{1'b0, port0_effective_address[31:11], port1_effective_address[31:11], 1'b0};
 
 endmodule
