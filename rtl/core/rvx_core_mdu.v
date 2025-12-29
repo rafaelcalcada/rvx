6a5bafdfc39f153ed84545ea25f978bce4ad6293
Rafael Calcada <rafaelcalcada@gmail.com>
2025-12-29 12:52:11 +0000

    Fix Verilator warnings and remove (un)signed wires (Vivado complains about it)

--- rtl/core/rvx_core_mdu.v
+++ rtl/core/rvx_core_mdu.v
@@ -5,30 +5,23 @@
 
 module rvx_core_mdu (
 
-	input wire [2:0] funct3_s2,
-	input wire [31:0] rs1_data_s2,
-	input wire [31:0] rs2_data_s2,
+    input wire [ 2:0] funct3_s2,
+    input wire [31:0] rs1_data_s2,
+    input wire [31:0] rs2_data_s2,
 
-	output reg [31:0] mdu_output_s2
+    output reg [31:0] mdu_output_s2
+
 );
 
-  wire signed   [31:0] rs1_s;
-  wire signed   [31:0] rs2_s;
-  wire unsigned [31:0] rs1_u;
-  wire unsigned [31:0] rs2_u;
+  wire [63:0] result_ss;
+  // verilator lint_off UNUSEDSIGNAL
+  wire [63:0] result_su;
+  wire [63:0] result_uu;
+  // verilator lint_on UNUSEDSIGNAL
 
-  wire signed   [63:0] result_ss;
-  wire signed   [63:0] result_su;
-  wire unsigned [63:0] result_uu;
-
-  assign rs1_s = $signed(rs1_data_s2);
-  assign rs2_s = $signed(rs2_data_s2);
-  assign rs1_u = $unsigned(rs1_data_s2);
-  assign rs2_u = $unsigned(rs2_data_s2);
-
-  assign result_ss = rs1_s * rs2_s;
-  assign result_su = rs1_s * $signed({1'b0, rs2_s});
-  assign result_uu = rs1_u * rs2_u;
+  assign result_ss = $signed(rs1_data_s2) * $signed(rs2_data_s2);
+  assign result_su = $signed(rs1_data_s2) * $signed({1'b0, rs2_data_s2});
+  assign result_uu = rs1_data_s2 * rs2_data_s2;
 
   always @* begin
     case (funct3_s2)
