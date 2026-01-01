b6f970b8245ecb5b781c543a5c0ae5fadb44f3db
Rafael Calcada <rafaelcalcada@gmail.com>
2026-01-01 16:24:47 +0000

    Refactoring of GPIO module

--- rtl/peripherals/rvx_gpio.v
+++ rtl/peripherals/rvx_gpio.v
@@ -1,75 +1,38 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2020-2025 RVX Project Contributors
 
+`include "rvx_constants.vh"
+
 module rvx_gpio #(
 
-    parameter GPIO_WIDTH = 1
+    parameter GPIO_WIDTH = 32
 
 ) (
 
     // Global signals
-
     input wire clock,
     input wire reset_n,
 
-    // IO interface
+    // Register read/write
+    input  wire [ 4:0] rw_address,
+    output reg  [31:0] read_data,
+    input  wire        read_request,
+    output reg         read_response,
+    input  wire [31:0] write_data,
+    input  wire [ 3:0] write_strobe,
+    input  wire        write_request,
+    output reg         write_response,
 
-    input  wire [           4:0] rw_address,
-    output reg  [          31:0] read_data,
-    input  wire                  read_request,
-    output reg                   read_response,
-    input  wire [GPIO_WIDTH-1:0] write_data,
-    input  wire [           3:0] write_strobe,
-    input  wire                  write_request,
-    output reg                   write_response,
-
-    // I/O signals
-
+    // GPIO signals
     input  wire [GPIO_WIDTH-1:0] gpio_input,
-    output wire [GPIO_WIDTH-1:0] gpio_oe,
-    output wire [GPIO_WIDTH-1:0] gpio_output
+    output reg  [GPIO_WIDTH-1:0] gpio_output_enable,
+    output reg  [GPIO_WIDTH-1:0] gpio_output
 
 );
 
-  localparam REG_IN = 5'h00;
-  localparam REG_OE = 5'h04;
-  localparam REG_OUT = 5'h08;
-  localparam REG_CLR = 5'h0C;
-  localparam REG_SET = 5'h10;
+  // Write and read responses
+  // ---------------------------------------------------------------------------
 
-
-  // Output Enable
-  reg [GPIO_WIDTH-1:0] oe;
-
-  // Output data
-  reg [GPIO_WIDTH-1:0] out;
-
-  assign gpio_oe     = oe;
-  assign gpio_output = out;
-
-  wire address_aligned = (~|rw_address[1:0]);
-  wire write_word = (&write_strobe);
-
-  always @(posedge clock) begin
-    if (!reset_n) begin
-      oe  <= {GPIO_WIDTH{1'b0}};
-      out <= {GPIO_WIDTH{1'b0}};
-    end
-    else if (write_request & address_aligned & write_word) begin
-      case (rw_address[4:0])
-        REG_OE:  oe <= write_data[0+:GPIO_WIDTH];
-        REG_OUT: out <= write_data[0+:GPIO_WIDTH];
-        REG_CLR: out <= out & ~write_data[0+:GPIO_WIDTH];
-        REG_SET: out <= out | write_data[0+:GPIO_WIDTH];
-        default: begin
-          oe  <= oe;
-          out <= out;
-        end
-      endcase
-    end
-  end
-
-  // Bus: Response to request
   always @(posedge clock) begin
     if (!reset_n) begin
       read_response  <= 1'b0;
@@ -81,23 +44,49 @@ module rvx_gpio #(
     end
   end
 
-  // Bus: Read registers
+  // Register read logic
+  // ---------------------------------------------------------------------------
+
   always @(posedge clock) begin
     if (!reset_n) begin
-      read_data <= 32'd0;
+      read_data <= 32'h00000000;
     end
-    else begin
-      if (read_request && address_aligned) begin
-        case (rw_address[4:0])
-          REG_IN:  read_data <= {{32 - GPIO_WIDTH{1'b0}}, (gpio_oe & out) | (~gpio_oe & gpio_input)};
-          REG_OE:  read_data <= {{32 - GPIO_WIDTH{1'b0}}, oe};
-          REG_OUT: read_data <= {{32 - GPIO_WIDTH{1'b0}}, out};
-          REG_CLR: read_data <= 32'd0;
-          REG_SET: read_data <= 32'd0;
-          default: begin
-          end
-        endcase
-      end
+    else if (read_request == 1'b1) begin
+      case (rw_address[4:0])
+        `RVX_GPIO_INPUT_REG_ADDR: begin
+          read_data <= {
+            {32 - GPIO_WIDTH{1'b0}}, (gpio_output_enable & gpio_output) | (~gpio_output_enable & gpio_input)
+          };
+        end
+        `RVX_GPIO_OUTPUT_ENABLE_REG_ADDR: read_data <= {{32 - GPIO_WIDTH{1'b0}}, gpio_output_enable};
+        `RVX_GPIO_OUTPUT_REG_ADDR:        read_data <= {{32 - GPIO_WIDTH{1'b0}}, gpio_output};
+        default:                          read_data <= 32'h00000000;
+      endcase
+    end
+  end
+
+  // Register write logic
+  // ---------------------------------------------------------------------------
+
+  wire valid_write_strobe = (write_strobe == 4'b1111 || write_strobe == 4'b0011 || write_strobe == 4'b0001);
+  wire valid_write_request = write_request == 1'b1 && valid_write_strobe;
+
+  always @(posedge clock) begin
+    if (!reset_n) begin
+      gpio_output_enable <= {GPIO_WIDTH{1'b0}};
+      gpio_output        <= {GPIO_WIDTH{1'b0}};
+    end
+    else if (valid_write_request == 1'b1) begin
+      case (rw_address[4:0])
+        `RVX_GPIO_OUTPUT_ENABLE_REG_ADDR: gpio_output_enable <= write_data[0+:GPIO_WIDTH];
+        `RVX_GPIO_OUTPUT_REG_ADDR:        gpio_output <= write_data[0+:GPIO_WIDTH];
+        `RVX_GPIO_CLEAR_REG_ADDR:         gpio_output <= gpio_output & ~write_data[0+:GPIO_WIDTH];
+        `RVX_GPIO_SET_REG_ADDR:           gpio_output <= gpio_output | write_data[0+:GPIO_WIDTH];
+        default: begin
+          gpio_output_enable <= gpio_output_enable;
+          gpio_output        <= gpio_output;
+        end
+      endcase
     end
   end
 
