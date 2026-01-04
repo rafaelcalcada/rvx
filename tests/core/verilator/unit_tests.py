7f9238e4942bcc6106e2d804d8607f81758e4fe4
Rafael Calcada <rafaelcalcada@gmail.com>
2026-01-04 00:57:32 +0000

    Fix path

--- tests/core/verilator/unit_tests.py
+++ tests/core/verilator/unit_tests.py
@@ -153,7 +153,7 @@ def main(argv=None):
 
     parser.add_argument('--sim',
                         type=str,
-                        default='verilated/rvx_simulator',
+                        default='../../../simulator/verilated/rvx_simulator',
                         help='Path to the simulator')
 
     parser.add_argument('--dump',
