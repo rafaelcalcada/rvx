5e704c17e61a48dfcbba1d6fe2d63328d70bedaf
Rafael Calcada <rafaelcalcada@gmail.com>
2026-01-24 22:02:38 +0000

    Refactoring of FreeRTOS example

---
+++ examples/freertos/rvx_freertos_example.c
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2020-2026 RVX Project Contributors
+
+#include "rvx.h"
+#include <FreeRTOS.h>
+#include <task.h>
+
+uint8_t ucHeap[configTOTAL_HEAP_SIZE]; ///< FreeRTOS statically allocated heap
+
+void led_0_task(void *pvParameters); ///< FreeRTOS Task for blinking LED 0
+void led_1_task(void *pvParameters); ///< FreeRTOS Task for blinking LED 1
+
+extern void freertos_risc_v_trap_handler(); ///< FreeRTOS trap handler
+
+void main(void)
+{
+  // Initialize UART at 9600 baud (assuming clock frequency is 12 MHz)
+  // Calculation: cycles_per_baud = CLOCK_FREQUENCY / UART_BAUD_RATE = 12000000 / 9600 = 1250
+  rvx_uart_init(RVX_UART_ADDRESS, 1250);
+
+  // Print welcome message
+  rvx_uart_write_string(RVX_UART_ADDRESS, "RVX FreeRTOS Example Project\n\n");
+  rvx_uart_write_string(
+      RVX_UART_ADDRESS,
+      "LEDs 0 and 1 will blink alternately at different rates with the help of FreeRTOS task scheduler.\n");
+
+  // Configure pins 0 and 2 as outputs
+  rvx_gpio_pin_configure(RVX_GPIO_ADDRESS, 0, RVX_GPIO_OUTPUT);
+  rvx_gpio_pin_configure(RVX_GPIO_ADDRESS, 2, RVX_GPIO_OUTPUT);
+
+  // Create 2 tasks for blinking LEDs
+  xTaskCreate(led_0_task, "LED 0 Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+  xTaskCreate(led_1_task, "LED 1 Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
+  // Start the FreeRTOS scheduler
+  vTaskStartScheduler();
+
+  while (1)
+  {
+  }
+}
+
+// FreeRTOS Task for blinking LED 0
+void led_0_task(void *pvParameters)
+{
+  (void)pvParameters;
+  for (;;)
+  {
+    vTaskDelay(pdMS_TO_TICKS(500)); // Delay 500 ms
+    rvx_gpio_pin_set(RVX_GPIO_ADDRESS, 0);
+    vTaskDelay(pdMS_TO_TICKS(500)); // Delay 500 ms
+    rvx_gpio_pin_clear(RVX_GPIO_ADDRESS, 0);
+  }
+}
+
+// FreeRTOS Task for blinking LED 1
+void led_1_task(void *pvParameters)
+{
+  (void)pvParameters;
+  for (;;)
+  {
+    vTaskDelay(pdMS_TO_TICKS(1000)); // Delay 1000 ms
+    rvx_gpio_pin_set(RVX_GPIO_ADDRESS, 2);
+    vTaskDelay(pdMS_TO_TICKS(1000)); // Delay 1000 ms
+    rvx_gpio_pin_clear(RVX_GPIO_ADDRESS, 2);
+  }
+}
+
+// Overrides RVX default trap handler implementation, delegating trap handling to FreeRTOS trap handler.
+RVX_NAKED void rvx_trap_handler_default(void)
+{
+  freertos_risc_v_trap_handler();
+}
+
+// This handler is called by FreeRTOS for all interrupts except timer interrupts.
+void freertos_risc_v_application_interrupt_handler(uint32_t interrupt_cause)
+{
+  (void)interrupt_cause;
+  rvx_uart_write_string(RVX_UART_ADDRESS, "Interrupt occurred!\n");
+
+  // Example
+
+  // if (interrupt_cause == RVX_TRAP_CAUSE_MEI) // Machine External Interrupt
+  // {
+  //   // Handle external interrupt
+  // }
+
+  return;
+}
+
+// This handler is called by FreeRTOS for all exceptions.
+void freertos_risc_v_application_exception_handler(uint32_t exception_cause)
+{
+  (void)exception_cause;
+  rvx_uart_write_string(RVX_UART_ADDRESS, "Exception occurred!\n");
+
+  // Example
+
+  // if (exception_cause == RVX_TRAP_CAUSE_BREAKPOINT)
+  // {
+  //   // Handle breakpoint exception
+  // }
+
+  return;
+}
