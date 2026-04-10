8a8e33ed62ca44d50e1f0745080497321a884351
Rafael Calcada <rafaelcalcada@gmail.com>
2026-04-10 20:52:02 +0000

    Update Timer function calls after changes in RVX HAL

--- tests/hal/source/rvx_hal_test_timer.c
+++ tests/hal/source/rvx_hal_test_timer.c
@@ -7,7 +7,7 @@
 /// @brief Set up the timer interrupt handler.
 RVX_TRAP_HANDLER_M(rvx_trap_handler_timer_m)
 {
-  rvx_timer_disable(RVX_TIMER_ADDRESS);
+  rvx_timer_stop(RVX_TIMER_ADDRESS);
   rvx_uart_send_string(RVX_UART_ADDRESS, "Passed.");
 }
 
@@ -20,14 +20,14 @@ void run_rvx_hal_timer_test()
   rvx_uart_send_string(RVX_UART_ADDRESS, "\nRVX HAL - Timer integration tests\n---------------------------------\n");
 
   rvx_test_start("\nTest 1: Timer COUNTER ENABLE register is 1 after reset. ");
-  RVX_TEST_ASSERT(rvx_timer_is_enabled(RVX_TIMER_ADDRESS) == true);
+  RVX_TEST_ASSERT(rvx_timer_is_running(RVX_TIMER_ADDRESS) == true);
   rvx_test_finish("(Passed)");
   rvx_test_update_error_count(&timer_tests_error_count);
 
-  rvx_test_start("\nTest 2: Timer COUNTER register is 0 after clear. ");
-  rvx_timer_disable(RVX_TIMER_ADDRESS);
-  rvx_timer_clear_counter(RVX_TIMER_ADDRESS);
-  RVX_TEST_ASSERT(rvx_timer_get_counter(RVX_TIMER_ADDRESS) == 0);
+  rvx_test_start("\nTest 2: Writing to the Timer COUNTER register sets the value correctly. ");
+  rvx_timer_stop(RVX_TIMER_ADDRESS);
+  rvx_timer_set_counter(RVX_TIMER_ADDRESS, 0x123456789ABCDEF0ULL);
+  RVX_TEST_ASSERT(rvx_timer_get_counter(RVX_TIMER_ADDRESS) == 0x123456789ABCDEF0ULL);
   rvx_test_finish("(Passed)");
   rvx_test_update_error_count(&timer_tests_error_count);
 
@@ -36,16 +36,17 @@ void run_rvx_hal_timer_test()
   rvx_test_finish("(Passed)");
   rvx_test_update_error_count(&timer_tests_error_count);
 
-  rvx_test_start("\nTest 4: Timer COUNTER register increments after enable. ");
-  rvx_timer_enable(RVX_TIMER_ADDRESS);
+  rvx_test_start("\nTest 4: Timer COUNTER register increments after start. ");
+  rvx_timer_set_counter(RVX_TIMER_ADDRESS, 0); // Reset counter to 0 before enabling
+  rvx_timer_start(RVX_TIMER_ADDRESS);
   for (volatile int i = 0; i < 100; i++) // Delay to allow counter to increment
     asm volatile("nop");
   RVX_TEST_ASSERT(rvx_timer_get_counter(RVX_TIMER_ADDRESS) > 0);
   rvx_test_finish("(Passed)");
   rvx_test_update_error_count(&timer_tests_error_count);
 
-  rvx_test_start("\nTest 5: Timer COUNTER register stops incrementing after disable. ");
-  rvx_timer_disable(RVX_TIMER_ADDRESS);
+  rvx_test_start("\nTest 5: Timer COUNTER register stops incrementing after stop. ");
+  rvx_timer_stop(RVX_TIMER_ADDRESS);
   uint64_t old_counter_value = rvx_timer_get_counter(RVX_TIMER_ADDRESS);
   for (volatile int i = 0; i < 100; i++) // Delay to check counter remains the same
     asm volatile("nop");
@@ -64,7 +65,7 @@ void run_rvx_hal_timer_test()
   rvx_test_start("\nTest 6: Check if timer interrupt is triggered. ");
   rvx_timer_set_counter(RVX_TIMER_ADDRESS, 0);
   rvx_timer_set_compare(RVX_TIMER_ADDRESS, 50); // Set compare to a small value
-  rvx_timer_enable(RVX_TIMER_ADDRESS);
+  rvx_timer_start(RVX_TIMER_ADDRESS);
   // Wait for some time to allow interrupt to trigger
   for (volatile int i = 0; i < 100; i++)
     asm volatile("nop");
