5afbcfe1e1659a5612d350e248dbdc6406aaf8f3
Rafael Calcada <rafaelcalcada@gmail.com>
2026-05-14 20:32:22 +0000

    Update I2C example

--- examples/i2c/rvx_i2c_example.c
+++ examples/i2c/rvx_i2c_example.c
@@ -12,27 +12,19 @@ void main(void)
 
   // Print welcome message
   rvx_uart_send_string(RVX_UART_ADDRESS, "RVX I2C Example Project\n\n");
-  rvx_uart_send_string(RVX_UART_ADDRESS, "This example reads the ID from BMP280 - Digital Pressure Sensor\n");
+  rvx_uart_send_string(RVX_UART_ADDRESS, "This example reads the ID from BME280 - Digital Pressure Sensor\n");
 
-  // Set I2C prescale to 10 kHz for 12 MHz
+  // Adjust I2C frequency by setting the clock divider (assuming 12 MHz clock, this sets I2C to 100 kHz)
   rvx_i2c_set_divider(RVX_I2C_ADDRESS, 1200);
 
-  const uint8_t bmp280_slave_addr = 0b1110110; // Or 0b1110111
+  const uint8_t bmp280_slave_addr = 0b1110110;
   const uint8_t bmp280_id_reg_addr = 0xD0;
-  uint8_t bmp280_id_value = 0u;
+  uint8_t bmp280_id_value = 0;
 
-  rvx_uart_send_string(RVX_UART_ADDRESS, "\nReading Manufacturer ID from I2C BMP280...\n");
-  // Start condition
-  rvx_i2c_run_start(RVX_I2C_ADDRESS);
-  // Write ID register address to BMP280
-  rvx_i2c_write_to(RVX_I2C_ADDRESS, bmp280_slave_addr, &bmp280_id_reg_addr, 1);
-  rvx_i2c_run_restart(RVX_I2C_ADDRESS);
-  // Read ID register from BMP280
-  rvx_i2c_reade_from(RVX_I2C_ADDRESS, bmp280_slave_addr, &bmp280_id_value, 1);
-  // Stop condition
-  rvx_i2c_run_stop(RVX_I2C_ADDRESS);
+  rvx_uart_send_string(RVX_UART_ADDRESS, "\nReading BME280 ID register over I2C...\n");
+  rvx_i2c_write_read(RVX_I2C_ADDRESS, bmp280_slave_addr, &bmp280_id_reg_addr, 1, &bmp280_id_value, 1);
 
-  // Print Manufacturer ID
+  // Print BME280 ID
   print_byte(bmp280_id_value);
 
   rvx_wait_for_interrupt();
