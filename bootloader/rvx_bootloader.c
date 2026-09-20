bb4db47cc4eb9b814e6261a2c736f07ce755188f
Rafael Calcada <rafaelcalcada@gmail.com>
2026-09-20 19:46:23 +0000

    Update bootloader generation

--- bootloader/rvx_bootloader.c
+++ bootloader/rvx_bootloader.c
@@ -1,67 +1,68 @@
-#include "rvx.h"
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2020-2026 RVX Project Contributors
 
-__attribute__((always_inline)) inline static uint32_t read_word_from_spi()
+#include <stdint.h>
+
+#define RVX_TCM_START_ADDRESS 0x00001000U
+#define RVX_MAGIC_NUMBER 0x52565830U
+#define RVX_SPI0_CHIP_SELECT_REG 0x40003004U
+#define RVX_SPI0_WRITE_REG 0x4000300CU
+#define RVX_SPI0_READ_REG 0x40003010U
+#define RVX_SPI0_STATUS_REG 0x40003014U
+
+static inline __attribute__((always_inline)) uint8_t rvx_bootloader_spi_transfer(const uint8_t tx_data)
 {
-  uint32_t word = 0;
-  for (int i = 0; i < 4; i++)
-  {
-    uint8_t byte = rvx_spi_transfer(RVX_SPI_ADDRESS, 0x00);
-    word = word | (((uint32_t)byte) << (i * 8));
-  }
+  *((volatile uint32_t *)(RVX_SPI0_WRITE_REG)) = tx_data;
+  while (*((volatile uint32_t *)(RVX_SPI0_STATUS_REG)) & 1)
+    ;
+  return (uint8_t)(*((volatile uint32_t *)(RVX_SPI0_READ_REG)));
+}
+
+static inline __attribute__((always_inline)) uint32_t rvx_bootloader_read_word_from_spi(void)
+{
+  uint32_t word = (uint32_t)rvx_bootloader_spi_transfer(0x00);
+  word |= ((uint32_t)rvx_bootloader_spi_transfer(0x00)) << 8;
+  word |= ((uint32_t)rvx_bootloader_spi_transfer(0x00)) << 16;
+  word |= ((uint32_t)rvx_bootloader_spi_transfer(0x00)) << 24;
   return word;
 }
 
-RVX_NAKED void rvx_bootloader(void)
+__attribute__((used, naked, section(".text.rvx_bootloader_entry"))) void rvx_bootloader(void)
 {
-  // Read CSR 0x7C0 (RVX SPI Boot Image Address)
-  uint32_t spi_boot_image_address;
-  asm volatile("csrr %0, 0x7C0" : "=r"(spi_boot_image_address));
+  *((volatile uint32_t *)(RVX_SPI0_CHIP_SELECT_REG)) = 0; // Assert CS
+  rvx_bootloader_spi_transfer(0x03);                      // READ_DATA command
+  rvx_bootloader_spi_transfer(0x00);                      // Address byte 0
+  rvx_bootloader_spi_transfer(0x00);                      // Address byte 1
+  rvx_bootloader_spi_transfer(0x00);                      // Address byte 2
 
-  uint8_t addr_byte_1 = (spi_boot_image_address >> 16) & 0xFF;
-  uint8_t addr_byte_2 = (spi_boot_image_address >> 8) & 0xFF;
-  uint8_t addr_byte_3 = spi_boot_image_address & 0xFF;
+  uint32_t rvx_magic = rvx_bootloader_read_word_from_spi();
 
-  rvx_spi_assert_cs(RVX_SPI_ADDRESS);
-  rvx_spi_write(RVX_SPI_ADDRESS, 0x03); // Read Data command
-  rvx_spi_write(RVX_SPI_ADDRESS, addr_byte_1);
-  rvx_spi_write(RVX_SPI_ADDRESS, addr_byte_2);
-  rvx_spi_write(RVX_SPI_ADDRESS, addr_byte_3);
-
-  uint32_t rv32_magic_number = read_word_from_spi();
-  uint32_t image_size = read_word_from_spi();
-  uint32_t entry_point = read_word_from_spi();
-  uint32_t rvx4_magic_number = read_word_from_spi();
-
-  uint32_t tcm_base_address = 0x00001000;
-
-  // If not a valid RVX boot image, try booting from TCM instead
-  if (rv32_magic_number != 0x52563332 || rvx4_magic_number != 0x52565834)
+  if (rvx_magic != RVX_MAGIC_NUMBER)
   {
-    rvx_spi_deassert_cs(RVX_SPI_ADDRESS);
-    rv32_magic_number = *((volatile uint32_t *)(tcm_base_address));
-    image_size = *((volatile uint32_t *)(tcm_base_address + 4));
-    entry_point = *((volatile uint32_t *)(tcm_base_address + 8));
-    rvx4_magic_number = *((volatile uint32_t *)(tcm_base_address + 12));
-    if (rv32_magic_number == 0x52563332 && rvx4_magic_number == 0x52565834)
-      asm volatile("jr %0" : : "r"(entry_point));
-    else
+    *((volatile uint32_t *)(RVX_SPI0_CHIP_SELECT_REG)) = 1; // Deassert CS
+    if (*((volatile uint32_t *)(RVX_TCM_START_ADDRESS)) != RVX_MAGIC_NUMBER)
+    {
       asm volatile("j 0x1000");
+    }
+
+    uint32_t entry_point = *((volatile uint32_t *)(RVX_TCM_START_ADDRESS + 4U));
+    asm volatile("jr %0" : : "r"(entry_point));
   }
 
-  // Write the image header
-  *((volatile uint32_t *)(tcm_base_address)) = rv32_magic_number;
-  *((volatile uint32_t *)(tcm_base_address + 4)) = image_size;
-  *((volatile uint32_t *)(tcm_base_address + 8)) = entry_point;
-  *((volatile uint32_t *)(tcm_base_address + 12)) = rvx4_magic_number;
+  uint32_t entry_point = rvx_bootloader_read_word_from_spi();
+  uint32_t image_size = rvx_bootloader_read_word_from_spi();
+  uint32_t riscv_magic = rvx_bootloader_read_word_from_spi();
 
-  // Write the image data
-  for (uint32_t i = 16; i < image_size; i += 4)
+  *((volatile uint32_t *)(RVX_TCM_START_ADDRESS)) = rvx_magic;
+  *((volatile uint32_t *)(RVX_TCM_START_ADDRESS + 4U)) = entry_point;
+  *((volatile uint32_t *)(RVX_TCM_START_ADDRESS + 8U)) = image_size;
+  *((volatile uint32_t *)(RVX_TCM_START_ADDRESS + 12U)) = riscv_magic;
+
+  for (uint32_t i = 16U; i < image_size; i += 4U)
   {
-    uint32_t word = read_word_from_spi();
-    *((volatile uint32_t *)(tcm_base_address + i)) = word;
+    *((volatile uint32_t *)(RVX_TCM_START_ADDRESS + i)) = rvx_bootloader_read_word_from_spi();
   }
-  rvx_spi_deassert_cs(RVX_SPI_ADDRESS);
 
-  // Jump to entry point
+  *((volatile uint32_t *)(RVX_SPI0_CHIP_SELECT_REG)) = 1; // Deassert CS
   asm volatile("jr %0" : : "r"(entry_point));
 }
